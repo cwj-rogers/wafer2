e041127533d83d90749cbedb6c8cb1fda30ef9e8
roger <cwjackk@gmail.com>
2018-03-26 01:40:01 +0800

    首次提交

---
+++ client/pages/home/home.js
@@ -0,0 +1,15 @@
+//index.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+    data: {
+        userInfo: {},
+        logged: false,
+        takeSession: false,
+        requestResult: ''
+    },
+
+    
+})
