7e34d2447b3692a8ccba2a0f12412046fc944e1a
cwjackk@163.com <cwjackk@163.com>
2018-03-26 18:05:21 +0800

    鍋ヨ韩瀹や粙缁嶉〉

--- client/pages/fitness/fitness.js
+++ client/pages/fitness/fitness.js
@@ -5,12 +5,15 @@ var util = require('../../utils/util.js')
 
 Page({
   data: {
-    background: ['../addCgi/code.jpg', 'demo-text-2', 'demo-text-3'],
+    imgUrls: [
+              'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+              'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+              'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
     indicatorDots: true,
-    vertical: false,
-    autoplay: false,
-    interval: 2000,
-    duration: 500
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
   changeIndicatorDots: function (e) {
     this.setData({
