e0e2eb81a238967c84e8780232d4ed57f0df88b5
cwjackk@163.com <cwjackk@163.com>
2018-03-29 23:45:11 +0800

    2018-3-29 场地详情页完善

--- client/pages/test/test.js
+++ client/pages/test/test.js
@@ -1,39 +1,31 @@
+var order = ['red', 'yellow', 'blue', 'green', 'red']
 Page({
   data: {
-    objectArray: [
-      { id: 5, unique: 'unique_5' },
-      { id: 4, unique: 'unique_4' },
-      { id: 3, unique: 'unique_3' },
-      { id: 2, unique: 'unique_2' },
-      { id: 1, unique: 'unique_1' },
-      { id: 0, unique: 'unique_0' },
-    ],
-    numberArray: [1, 2, 3, 4]
+    toView: 'red',
+    scrollTop: 0
   },
-  switch: function (e) {
-    const length = this.data.objectArray.length
-    for (let i = 0; i < length; ++i) {
-      const x = Math.floor(Math.random() * length)
-      const y = Math.floor(Math.random() * length)
-      const temp = this.data.objectArray[x]
-      this.data.objectArray[x] = this.data.objectArray[y]
-      this.data.objectArray[y] = temp
+  upper: function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
     }
-    this.setData({
-      objectArray: this.data.objectArray
-    })
   },
-  addToFront: function (e) {
-    const length = this.data.objectArray.length
-    this.data.objectArray = [{ id: length, unique: 'unique_' + length }].concat(this.data.objectArray)
+  tapMove: function (e) {
     this.setData({
-      objectArray: this.data.objectArray
-    })
-  },
-  addNumberToFront: function (e) {
-    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
-    this.setData({
-      numberArray: this.data.numberArray
+      scrollTop: this.data.scrollTop + 10
     })
   }
 })
