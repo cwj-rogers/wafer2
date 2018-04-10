0e5e4828f6a2931cdd431a4e84dc12bc92d58095
roger <cwjackk@gmail.com>
2018-04-11 02:30:35 +0800

    完成选择日期功能

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -9,8 +9,9 @@ Page({
       logged: false,
       takeSession: false,
       requestResult: '',
-      week: ["日", "一", "二", "三", "四", "五", "六"],
-      state: "0",
+      month: getMonth(),
+      weekDate: getWeekDate(),
+      selectedIndex: 0,
   },
   joinAct:function(e){
     console.log("参加");
@@ -31,7 +32,39 @@ Page({
       url: '../fitness/fitness'
     })
   },
-  chooseDate: function(){
-
+  dateButtonTap: function(e){
+    // console.log(e);
+    var index = e.currentTarget.dataset.index;
+    this.setData({
+      selectedIndex: index,
+    });
   }
 })
+
+//获取格式化时间数据
+function getWeekDate(){
+  var date = new Date();
+  var data = [{ },{ },{ },{ },{ },{ },{ }];
+  var week = date.getDay();
+  for(var i=0;i<data.length;i++){
+    var key = week+i>6? week+i-7:week+i;
+    data[i].week = ["日", "一", "二", "三", "四", "五", "六"][key];
+    data[i].day = getDay(i)+"号";
+  }
+  return data;
+}
+//获取未来7天日期
+function getDay(day) {
+  var today = new Date();
+
+  var targetday_milliseconds = today.getTime() + 1000 * 60 * 60 * 24 * day;
+
+  today.setTime(targetday_milliseconds); //注意，这行是关键代码  
+
+  return today.getDate();
+}    
+//获取月份
+function getMonth(){
+  var month = (new Date().getMonth()+1)+"月";
+  return month.toString();
+}
